100914a036c0c99316895b2d1c3497e0ace1f05b
mda2bj <mda2bj@bosch.com>
2026-07-02 15:00:13 +0200

    news

--- linux/apps.sh
+++ linux/apps.sh
@@ -18,9 +18,16 @@ apps=(
 sudo apt-get update
 sudo apt-get install -y "${apps[@]}"
 
+sudo apt-get upgrade -y
 
+# UV
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
+# Node.js
 curl https://get.volta.sh | bash
 
+# Azure CLI
+curl -fsSL 'https://azurecliprod.blob.core.windows.net/$root/deb_install.sh' | sudo bash
+
+# Update autocompletions for fish shell
 fish_update_completions
